410a4471d727123c57dd47efe152b4e0c5da9686
systm32 <rajatbajaj56@gmail.com>
2016-06-15 17:29:26 +0530

    Update config.php

--- login/config.php
+++ login/config.php
@@ -3,8 +3,8 @@ session_start();
 include_once("src/Google_Client.php");
 include_once("src/contrib/Google_Oauth2Service.php");
 ######### edit details ##########
-$clientId = '325931357839-ug0qig4gs4duud1l77b5n7h2i0fgjmas.apps.googleusercontent.com'; //Google CLIENT ID
-$clientSecret = 'cAwS8dyTDHggrzmPUvfZlrtx'; //Google CLIENT SECRET
+$clientId = 'Client id of your project'; //Google CLIENT ID
+$clientSecret = 'Client Secret of your project'; //Google CLIENT SECRET
 $redirectUrl = 'http://localhost:8000/login';  //return url (url to script)
 $homeUrl = 'http://localhost:8000/login';  //return to home
 $projectUrl = '';
